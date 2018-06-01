7e9183898a6976a82e29ff0d13c41cf7119602de
Israel Manzo <israelmanzo814@gmail.com>
2018-06-01 15:23:20 -0400

    Updating tabbar divider line

--- BuildingMng/BuildingMng/Controller/CustomTabBarController.swift
+++ BuildingMng/BuildingMng/Controller/CustomTabBarController.swift
@@ -24,6 +24,14 @@ class CustomTabBarController: UITabBarController {
         secondTabVC.tabBarItem.image = UIImage(named: "galleries")
         
         viewControllers = [nav, secondTabVC]
+        
+        tabBar.isTranslucent = false
+        
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
+        border.backgroundColor = UIColor(red: 229/255, green: 231/255, blue: 235/255, alpha: 1).cgColor
+        tabBar.layer.addSublayer(border)
+        tabBar.clipsToBounds = true
     }
 }
 
