d2fa3951e788283d44ae62cce9e4aa799a35c0e2
Israel Manzo <israelmanzo814@gmail.com>
2018-06-02 12:36:42 -0400

    Updating collection gallery

--- BuildingMng/BuildingMng/Controller/GalleryDisplay.swift
+++ BuildingMng/BuildingMng/Controller/GalleryDisplay.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class GalleryDisplay: UIViewController {
+class GalleryDisplay: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 12
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .blue
+        return cv
+    }()
+    
+    private let displayCell = "displayCell"
     
     var displayGalleries: BuildingGallery?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        print(displayGalleries ?? 0)
+        setView()
+        guard let display = displayGalleries?.gallery,
+              let title = displayGalleries?.title else {return}
+    }
+    
+    func setView(){
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: displayCell)
+        
+        collectionView.setAnchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: displayCell, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 345, height: 450)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 50)
     }
 }
+
+
+
+
+
+
+
+
+
+
