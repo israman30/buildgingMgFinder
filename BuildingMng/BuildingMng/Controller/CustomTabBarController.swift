ff9af1438f6876fa102016b77f45aa92b95052c0
Israel Manzo <israelmanzo814@gmail.com>
2018-06-01 21:25:40 -0400

    Adding info view controller

--- BuildingMng/BuildingMng/Controller/CustomTabBarController.swift
+++ BuildingMng/BuildingMng/Controller/CustomTabBarController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+/* ================= INFO ===================
+ 
+ Tab Bar Controller
+ - Extended view controller in an array to display on the tab bar
+ 
+ ============================================ */
+
+
 class CustomTabBarController: UITabBarController {
     
     override func viewDidLoad() {
@@ -23,10 +31,16 @@ class CustomTabBarController: UITabBarController {
         secondTabVC.title = "gallery"
         secondTabVC.tabBarItem.image = UIImage(named: "galleries")
         
-        viewControllers = [nav, secondTabVC]
+        let info = UIViewController()
+        let thirdVC = UINavigationController(rootViewController: info)
+        thirdVC.title = "info"
+        thirdVC.tabBarItem.image = UIImage(named: "information")
+        
+        viewControllers = [nav, secondTabVC, thirdVC]
         
         tabBar.isTranslucent = false
         
+        // Custom border view on top of tab bar
         let border = CALayer()
         border.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
         border.backgroundColor = UIColor(red: 229/255, green: 231/255, blue: 235/255, alpha: 1).cgColor
