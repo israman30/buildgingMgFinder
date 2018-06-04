61161abeff33175eaff0bf4f8c71a69e801274a9
Israel Manzo <israelmanzo814@gmail.com>
2018-06-04 09:39:16 -0400

    Updating view cell

--- BuildingMng/BuildingMng/Controller/Gallery/GalleryVC.swift
+++ BuildingMng/BuildingMng/Controller/Gallery/GalleryVC.swift
@@ -35,7 +35,7 @@ class GalleryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let tableView: UITableView = {
         let tv = UITableView()
-        tv.rowHeight = 50
+        tv.rowHeight = 150
         return tv
     }()
     
@@ -57,7 +57,7 @@ class GalleryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cell)
+        tableView.register(GalleryCell.self, forCellReuseIdentifier: cell)
         
         
         tableView.setAnchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
@@ -72,9 +72,12 @@ class GalleryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.cell)
-        cell?.textLabel?.text = galleryInfo[indexPath.row].title
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cell) as! GalleryCell
+        
+        cell.titleLabel.text = galleryInfo[indexPath.row].title
+        
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -83,4 +86,47 @@ class GalleryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         navigationController?.pushViewController(galleryDisplay, animated: true)
     }
     
+}
+
+class GalleryCell: UITableViewCell {
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = . clear
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 5, height: 5)
+        view.layer.shadowRadius = 5
+        view.layer.shadowOpacity = 0.5
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: "cell")
+        addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+        
+        containerView.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10)
+        
+        titleLabel.setAnchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingBottom: 0, paddingRight: 20)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
