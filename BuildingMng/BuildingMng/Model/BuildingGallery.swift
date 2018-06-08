d6c8926146c7c8073019c597bff6c32352cc32be
Israel Manzo <israelmanzo814@gmail.com>
2018-06-08 09:50:47 -0400

    Updating list gallery

--- BuildingMng/BuildingMng/Model/BuildingGallery.swift
+++ BuildingMng/BuildingMng/Model/BuildingGallery.swift
@@ -24,9 +24,9 @@ struct BuildingGallery {
             
             BuildingGallery(title: "THE RIVINGTON", gallery: ["riv1", "riv2", "riv3", "riv5", "riv6", "riv7"], address: "1130 Grand St, Hoboken, NJ 07030"),
             
-            BuildingGallery(title: "OBSERVER PARK", gallery: ["artisan1", "artisan2", "artisan3", "artisan4", "artisan5", "artisan6"], address: "51 Garden St, Hoboken, NJ 07030"),
+            BuildingGallery(title: "OBSERVER PARK", gallery: ["ob1", "ob2", "ob3", "ob4", "ob5"], address: "51 Garden St, Hoboken, NJ 07030"),
             
-            BuildingGallery(title: "HARLLOW", gallery: ["artisan1", "artisan2", "artisan3", "artisan4", "artisan5", "artisan6"], address: "1330 Willow Ave, Hoboken, NJ 07030"),
+            BuildingGallery(title: "HARLLOW", gallery: ["hard1", "hard2", "hard3", "hard4", "hard5", "hard6", "hard7", "hard8", "hard9", "hard10"], address: "1330 Willow Ave, Hoboken, NJ 07030"),
             
             BuildingGallery(title: "THE SOVEREIGN", gallery: ["artisan1", "artisan2", "artisan3", "artisan4", "artisan5", "artisan6"], address: "2 14th St, Hoboken, NJ 07030"),
             
