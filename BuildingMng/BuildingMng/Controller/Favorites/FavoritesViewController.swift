0a984711c1954c8e5ff8133f8c2ad4505367d906
Israel Manzo <israelmanzo814@gmail.com>
2018-06-10 14:22:58 -0400

    Adding favorite view controller

---
+++ BuildingMng/BuildingMng/Controller/Favorites/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  BuildingMng
+//
+//  Created by Israel Manzo on 6/10/18.
+//  Copyright © 2018 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
