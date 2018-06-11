1307391eb17384f3cfa2f128bcb77752a53f7b3d
Israel Manzo <israelmanzo814@gmail.com>
2018-06-10 22:44:35 -0400

    Updating tableview cell loading info

--- BuildingMng/BuildingMng/Controller/Favorites/FavoritesViewController.swift
+++ BuildingMng/BuildingMng/Controller/Favorites/FavoritesViewController.swift
@@ -8,23 +8,54 @@
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let tableView: UITableView = {
         let tv = UITableView()
-        tv.rowHeight = 150
+        tv.rowHeight = 50
         tv.showsVerticalScrollIndicator = false
         return tv
     }()
     
     var favorites: BuildingGallery!
+    var containerFav = [BuildingGallery]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
+        setFavoritesView()
+        insertNewFavoriteItem()
+    }
+    
+    func insertNewFavoriteItem(){
+        containerFav.append(favorites)
         
+        let indexPath = IndexPath(row: containerFav.count - 1, section: 0)
+        
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        tableView.endUpdates()
+    }
+    
+    fileprivate func setFavoritesView() {
         view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "favCell")
         tableView.setAnchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        print(favorites)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return (favorites.title?.count)!
+        return containerFav.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favCell")
+//        cell?.textLabel?.text = favorites.title
+        cell?.textLabel?.text = containerFav[indexPath.row].title
+        
+        return cell!
     }
 }
