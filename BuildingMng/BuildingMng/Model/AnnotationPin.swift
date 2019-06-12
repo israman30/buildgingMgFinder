0cee8e90bb86dd2d979160ccf8dfd6cc16e15aa6
Israel Manzo <israelmanzo814@gmail.com>
2019-06-12 13:09:46 -0400

    Updated annotation class

--- BuildingMng/BuildingMng/Model/AnnotationPin.swift
+++ BuildingMng/BuildingMng/Model/AnnotationPin.swift
@@ -32,31 +32,3 @@ class AnnotationPin: NSObject, MKAnnotation {
     
 }
 
-class Annotation: NSObject, MKAnnotation {
-    let title: String?
-    let locationName: String
-    let discipline: String
-    let coordinate: CLLocationCoordinate2D
-    
-    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
-        self.title = title
-        self.locationName = locationName
-        self.discipline = discipline
-        self.coordinate = coordinate
-        
-        super.init()
-    }
-    
-    var subtitle: String? {
-        return locationName
-    }
-    
-    // Annotation right callout accessory opens this mapItem in Maps app
-    func mapItem() -> MKMapItem {
-        let addressDict = [CNPostalAddressStreetKey: subtitle!]
-        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = title
-        return mapItem
-    }
-}
